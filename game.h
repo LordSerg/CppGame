39cc9363e201f5d31834edf6b1fdfc8169e0faff
LordSerg <oknelaksoms@gmail.com>
2024-10-10 23:47:30 +0300

    idea of new architecture

--- game.h
+++ game.h
@@ -13,12 +13,12 @@
 #include <tuple>
 #include <bits/stdc++.h>
 
-/*is for everyting that a gamer has*/
+/*is for everyting that a player has*/
 class Game
 {
 	
 	/*in-game staff*/
-	static map *m;
+	map &m;
 	std::vector<*Iunit> units;
 	std::vector<*Iunit> selectedUnits;
 	std::vector<*Ibuilding> building;
@@ -41,22 +41,11 @@ public:
 	void SelectUnits(int _x0, int _y0, int _x1, int _y1);
 	void SelectBuilding(int _x, int _y);
 	
-	/*for moving units on the map*/
+	/*for setting tasks for units*/
 	void goTo(int _x, int _y);//move selected unit(s) to the location
-private:
-	std::vector<std::tuple<int, int>> findPath(map m, int x_0, int y_0, int x_1, int y_1);
-
-public:
-	/*every unit can fight, so here is function that ask selected units to go to fight*/
-	void fight(int _x, int _y); // fight some location (if there is something stands)
-private:
-	void fight(Iunit* unit_to_fight); // fight specific unit
-	void fight(Ibuilding* building_to_destroy); // fight specific building
-	
-public:
-	/*build something*/
-	void build(Ibuilding* building);
-	void mine(int _x, int _y);
+	void build(int _x, int _y, Ibuilding *building);
+	void mine(int _x0, int _y0, int _x1, int _y1);
+	void fight(Iunit &other);
 	
 	/*to play a game - you have to select a game*/
 	void LoadGame(std::string path_to_game); //load saved game from file
